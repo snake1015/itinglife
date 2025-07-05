04d3465033cb0aabeba1bfb4aaa38489166227b0
snake89 <qjxking@126.com>
2025-07-05 12:12:33 +0800

    修复Vue模板中的箭头函数语法错误，解决构建问题

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,12 @@
   <el-container style="min-height: 100vh;">
     <el-header>
       <el-menu :default-active="$route.path" mode="horizontal">
-        <el-menu-item index="/" @click="() => go('/')">首页</el-menu-item>
-        <el-menu-item index="/tech" @click="() => go('/tech')">技术分享</el-menu-item>
-        <el-menu-item index="/life" @click="() => go('/life')">生活分享</el-menu-item>
-        <el-menu-item index="/about" @click="() => go('/about')">关于</el-menu-item>
-        <el-menu-item index="/contact" @click="() => go('/contact')">联系我们</el-menu-item>
-        <el-menu-item index="/admin" @click="() => go('/admin')">后台管理</el-menu-item>
+        <el-menu-item index="/" @click="go('/')">首页</el-menu-item>
+        <el-menu-item index="/tech" @click="go('/tech')">技术分享</el-menu-item>
+        <el-menu-item index="/life" @click="go('/life')">生活分享</el-menu-item>
+        <el-menu-item index="/about" @click="go('/about')">关于</el-menu-item>
+        <el-menu-item index="/contact" @click="go('/contact')">联系我们</el-menu-item>
+        <el-menu-item index="/admin" @click="go('/admin')">后台管理</el-menu-item>
       </el-menu>
     </el-header>
     <el-main>
