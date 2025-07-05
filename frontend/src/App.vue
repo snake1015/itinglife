1f28db4de9414ba08679a559b6325774ce9702f0
snake89 <qjxking@126.com>
2025-07-05 12:18:19 +0800

    重构App.vue中的事件处理，避免Vite解析错误

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,12 @@
   <el-container style="min-height: 100vh;">
     <el-header>
       <el-menu :default-active="$route.path" mode="horizontal">
-        <el-menu-item index="/" @click="go('/')">首页</el-menu-item>
-        <el-menu-item index="/tech" @click="go('/tech')">技术分享</el-menu-item>
-        <el-menu-item index="/life" @click="go('/life')">生活分享</el-menu-item>
-        <el-menu-item index="/about" @click="go('/about')">关于</el-menu-item>
-        <el-menu-item index="/contact" @click="go('/contact')">联系我们</el-menu-item>
-        <el-menu-item index="/admin" @click="go('/admin')">后台管理</el-menu-item>
+        <el-menu-item index="/" @click="goHome">首页</el-menu-item>
+        <el-menu-item index="/tech" @click="goTech">技术分享</el-menu-item>
+        <el-menu-item index="/life" @click="goLife">生活分享</el-menu-item>
+        <el-menu-item index="/about" @click="goAbout">关于</el-menu-item>
+        <el-menu-item index="/contact" @click="goContact">联系我们</el-menu-item>
+        <el-menu-item index="/admin" @click="goAdmin">后台管理</el-menu-item>
       </el-menu>
     </el-header>
     <el-main>
@@ -19,7 +19,28 @@
 <script setup>
   import { useRouter } from 'vue-router';
   const router = useRouter();
-  function go(path) {
-    router.push(path);
+  
+  function goHome() {
+    router.push('/');
+  }
+  
+  function goTech() {
+    router.push('/tech');
+  }
+  
+  function goLife() {
+    router.push('/life');
+  }
+  
+  function goAbout() {
+    router.push('/about');
+  }
+  
+  function goContact() {
+    router.push('/contact');
+  }
+  
+  function goAdmin() {
+    router.push('/admin');
   }
 </script> 
