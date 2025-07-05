30fbf4ec810c484e2128c525c405d019d33c857a
snake89 <qjxking@126.com>
2025-07-05 12:22:27 +0800

    简化App.vue事件处理，直接使用router.push避免解析错误

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,12 @@
   <el-container style="min-height: 100vh;">
     <el-header>
       <el-menu :default-active="$route.path" mode="horizontal">
-        <el-menu-item index="/" @click="goHome">首页</el-menu-item>
-        <el-menu-item index="/tech" @click="goTech">技术分享</el-menu-item>
-        <el-menu-item index="/life" @click="goLife">生活分享</el-menu-item>
-        <el-menu-item index="/about" @click="goAbout">关于</el-menu-item>
-        <el-menu-item index="/contact" @click="goContact">联系我们</el-menu-item>
-        <el-menu-item index="/admin" @click="goAdmin">后台管理</el-menu-item>
+        <el-menu-item index="/" @click="router.push('/')">首页</el-menu-item>
+        <el-menu-item index="/tech" @click="router.push('/tech')">技术分享</el-menu-item>
+        <el-menu-item index="/life" @click="router.push('/life')">生活分享</el-menu-item>
+        <el-menu-item index="/about" @click="router.push('/about')">关于</el-menu-item>
+        <el-menu-item index="/contact" @click="router.push('/contact')">联系我们</el-menu-item>
+        <el-menu-item index="/admin" @click="router.push('/admin')">后台管理</el-menu-item>
       </el-menu>
     </el-header>
     <el-main>
@@ -19,28 +19,4 @@
 <script setup>
   import { useRouter } from 'vue-router';
   const router = useRouter();
-  
-  function goHome() {
-    router.push('/');
-  }
-  
-  function goTech() {
-    router.push('/tech');
-  }
-  
-  function goLife() {
-    router.push('/life');
-  }
-  
-  function goAbout() {
-    router.push('/about');
-  }
-  
-  function goContact() {
-    router.push('/contact');
-  }
-  
-  function goAdmin() {
-    router.push('/admin');
-  }
 </script> 
