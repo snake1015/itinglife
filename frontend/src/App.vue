43401fae571671850ec32b70d1cd15ae252938a2
snake89 <qjxking@126.com>
2025-07-05 12:00:14 +0800

    fix: el-menu navigation, remove router attribute and use click for navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <el-container style="min-height: 100vh;">
     <el-header>
-      <el-menu :default-active="$route.path" mode="horizontal" router>
-        <el-menu-item index="/">首页</el-menu-item>
-        <el-menu-item index="/tech">技术分享</el-menu-item>
-        <el-menu-item index="/life">生活分享</el-menu-item>
-        <el-menu-item index="/about">关于</el-menu-item>
-        <el-menu-item index="/contact">联系我们</el-menu-item>
-        <el-menu-item index="/admin">后台管理</el-menu-item>
-      </el-menu>
+      <el-menu :default-active="$route.path" mode="horizontal">
+      <el-menu-item index="/" @click="go('/')">首页</el-menu-item>
+      <el-menu-item index="/tech" @click="go('/tech')">技术分享</el-menu-item>
+      <el-menu-item index="/life" @click="go('/life')">生活分享</el-menu-item>
+      <el-menu-item index="/about" @click="go('/about')">关于</el-menu-item>
+      <el-menu-item index="/contact" @click="go('/contact')">联系我们</el-menu-item>
+      <el-menu-item index="/admin" @click="go('/admin')">后台管理</el-menu-item>
+    </el-menu>
     </el-header>
     <el-main>
       <router-view />
@@ -17,4 +17,9 @@
 </template>
 
 <script setup>
+  import { useRouter } from 'vue-router';
+  const router = useRouter();
+  function go(path) {
+    router.push(path);
+  }
 </script> 
